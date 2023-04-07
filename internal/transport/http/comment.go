837c8454812d2943e189eb9dadc850fa87f4d821
Seleroad <118297089+Solblnc@users.noreply.github.com>
2023-04-07 19:35:49 +0300

    JWT auth + logic

--- internal/transport/http/comment.go
+++ internal/transport/http/comment.go
@@ -3,24 +3,47 @@ package http
 import (
 	"RestApi2.0/internal/comment"
 	"encoding/json"
+	"github.com/go-playground/validator/v10"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
+type PostComment struct {
+	Slug   string `json:"slug" validate:"required"`
+	Body   string `json:"body" validate:"required"`
+	Author string `json:"author" validate:"required"`
+}
+
+func ConvertPostCommentToComment(c PostComment) comment.Comment {
+	return comment.Comment{
+		Slug:   c.Slug,
+		Body:   c.Body,
+		Author: c.Author,
+	}
+}
+
 func (h *Handler) PostComment(w http.ResponseWriter, r *http.Request) {
-	var cmt comment.Comment
+	var cmt PostComment
 	if err := json.NewDecoder(r.Body).Decode(&cmt); err != nil {
 		return
 	}
 
-	cmt, err := h.Service.PostComment(r.Context(), cmt)
+	validate := validator.New()
+	err := validate.Struct(cmt)
+	if err != nil {
+		http.Error(w, "not valid request", http.StatusBadRequest)
+		return
+	}
+	convertedComment := ConvertPostCommentToComment(cmt)
+
+	postedComment, err := h.Service.PostComment(r.Context(), convertedComment)
 	if err != nil {
 		log.Print(err)
 		return
 	}
 
-	if err = json.NewEncoder(w).Encode(cmt); err != nil {
+	if err = json.NewEncoder(w).Encode(postedComment); err != nil {
 		panic(err)
 	}
 }
